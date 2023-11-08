563c8bfd3c544135aa3fd929c07ffb1057b2e3ab
Bhuvneshwar Singh <143737224+SinghBhuvneshwar@users.noreply.github.com>
2023-11-08 15:26:05 +0530

    Create install-agent.sh

---
+++ install-agent.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+# This installs the CodeDeploy agent and its prerequisites on Ubuntu 22.04. 
+# change region in line 7 according to your preferable region.
+sudo apt-get update 
+sudo apt-get install ruby-full ruby-webrick wget -y 
+cd /tmp 
+wget https://aws-codedeploy-region.s3.region.amazonaws.com/releases/codedeploy-agent_1.3.2-1902_all.deb 
+mkdir codedeploy-agent_1.3.2-1902_ubuntu22 
+dpkg-deb -R codedeploy-agent_1.3.2-1902_all.deb codedeploy-agent_1.3.2-1902_ubuntu22 
+sed 's/Depends:.*/Depends:ruby3.0/' -i ./codedeploy-agent_1.3.2-1902_ubuntu22/DEBIAN/control 
+dpkg-deb -b codedeploy-agent_1.3.2-1902_ubuntu22/ 
+sudo dpkg -i codedeploy-agent_1.3.2-1902_ubuntu22.deb 
+systemctl list-units --type=service | grep codedeploy 
+sudo service codedeploy-agent statu
